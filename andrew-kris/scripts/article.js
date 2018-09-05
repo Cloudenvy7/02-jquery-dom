9ebcbe07a110721b1db31938f403652d52f780f7
Andrew <andrewscottpowers@gmail.com>
2018-09-05 10:38:49 -0700

    just got infromation appended

--- andrew-kris/scripts/article.js
+++ andrew-kris/scripts/article.js
@@ -8,20 +8,18 @@ let articles = [];
 function Article (rawDataObj) {
   // TODO: Use the JS object that is passed in to complete this constructor function:
   // Save ALL the properties of `rawDataObj` into `this`
-  rawDataObj.array.forEach(function(title, category, author, authorUrl, publishedOn, body) {
-    this.title = title;
-    this.category = category;
-    this.author = author;
-    this.authorUrl = authorUrl;
-    this.publishedOn = publishedOn;
-    this.body = body;
-    articles.push(this);
-  });
+  this.title = rawDataObj.title;
+  this.category = rawDataObj.category;
+  this.author = rawDataObj.author;
+  this.authorUrl = rawDataObj.authorUrl;
+  this.publishedOn = rawDataObj.publishedOn;
+  this.body = rawDataObj.body;
+ 
 }
 
 Article.prototype.toHtml = function() {
   // COMMENT: What is the benefit of cloning the article? (see the jQuery docs)
-  // The benefit of cloning the article is that we can add whole articles rather than just pieces of the article. 
+  // The benefit of cloning the article is that we can add whole articles rather than just pieces of the article.
 
   let $newArticle = $('article.template').clone();
   /* TODO: This cloned article still has a class of template. In our modules.css stylesheet, we should give all elements with a class of template a display of none so that our template does not display in the browser. But, we also need to make sure we're not accidentally hiding our cloned article. */
@@ -29,7 +27,16 @@ Article.prototype.toHtml = function() {
   if (!this.publishedOn) $newArticle.addClass('draft');
   $newArticle.attr('data-category', this.category);
 
-  /* TODO: Now use jQuery traversal and setter methods to fill in the rest of the current template clone with values of the properties of this particular Article instance.
+  $newArticle.append($('h2').html(this.title));
+  $newArticle.append($('h3').html(this.author));
+  $newArticle.append($('h4').html(this.publishedOn));
+  $newArticle.append($('p').html(this.body));
+  $newArticle.append($('p').html(this.authorUrl));
+  
+  
+
+  /* TODO: Now use jQuery traversal and setter methods to fill in the rest of the current template clone with values of the properties of this particular A
+  rticle instance.
     We need to fill in:
       1. author name,
       2. author url,
